3c3b56100db93c6ffd93a8abf652a51e3821faa4
Marissa Nolan <ritz1484@cs.com>
2021-01-21 23:52:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,35 @@
 # Write your code here!
+def game_hash
+  hash = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [{:player_name => "", 
+      :number => num,
+      :shoe => ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks =>
+      }]
+    }
+    
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => [{:player_name => "", 
+      :number => num,
+      :shoe => ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks =>
+      }]
+    }
+    
+  }
+end
